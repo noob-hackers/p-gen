4b9e693a64704ce9e6924cea939a9ff3ca30f14a
jaxparrow07 <jackandrichardjunior@gmail.com>
2021-01-06 19:13:21 +0530

    Read only Password output box

--- p-gen-gui.py
+++ p-gen-gui.py
@@ -7,10 +7,11 @@ from PyQt5.QtGui import QIcon, QFont, QClipboard, QIntValidator
 from qt_material import apply_stylesheet
 
 # Maximum length
-maxchar = 1500
+maxchar: int = 1500
 
 # Characters
-char="abcdefghijklmnopqrstuvwxyz1234567890@#$%&*^"
+char: str = "abcdefghijklmnopqrstuvwxyz1234567890@#$%&*^"
+
 
 class Example(QMainWindow):
 
@@ -22,29 +23,29 @@ class Example(QMainWindow):
     def initUI(self):
 
         self.passlen = QLabel('Password Length', self)
-        self.passlen.move(128,43)
+        self.passlen.move(128, 43)
         self.passlen.adjustSize()
 
-        self.Title = QLabel('P-GEN GUI',self)
-        self.Title.move(120,10)
+        self.Title = QLabel('P-GEN GUI', self)
+        self.Title.move(120, 10)
         self.Title.setFont(QFont('Arial', 19))
         self.Title.adjustSize()
 
         generate = QPushButton("Generate", self)
-        generate.setGeometry(113,107,130,40)
+        generate.setGeometry(113, 107, 130, 40)
         generate.setIcon(QIcon('gui/generate.png'))
         generate.clicked.connect(self.Generate)
 
-        minusicon = QPushButton("-",self)
-        minusicon.setGeometry(95,70,40,40)
+        minusicon = QPushButton("-", self)
+        minusicon.setGeometry(95, 70, 40, 40)
         minusicon.clicked.connect(self.decrease)
 
-        plusicon = QPushButton("+",self)
-        plusicon.setGeometry(209,70,40,40)
+        plusicon = QPushButton("+", self)
+        plusicon.setGeometry(209, 70, 40, 40)
         plusicon.clicked.connect(self.increase)
 
         copypass = QPushButton('Copy', self)
-        copypass.move(128,253)
+        copypass.move(128, 253)
         copypass.setIcon(QIcon('gui/copy.png'))
         copypass.clicked.connect(self.copy)
 
@@ -56,46 +57,43 @@ class Example(QMainWindow):
         self.charlen.setValidator(self.onlyInt)
 
         self.passwordout = QPlainTextEdit(self)
-        self.passwordout.setGeometry(60,149,240,100)
+        self.passwordout.setGeometry(60, 149, 240, 100)
+        self.passwordout.setReadOnly(True)
 
         self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle('P-GEN GUI')
         self.show()
 
     def increase(self):
-    	if int(self.charlen.text()) < maxchar:
-    		self.charlen.setText(str(int(self.charlen.text())+1))
-    	else:
-    		self.charlen.setText(str(maxchar))
+        if int(self.charlen.text()) < maxchar:
+            self.charlen.setText(str(int(self.charlen.text()) + 1))
+        else:
+            self.charlen.setText(str(maxchar))
 
     def decrease(self):
-    	if int(self.charlen.text()) > 2:
-    		self.charlen.setText(str(int(self.charlen.text())-1))
-    	else:
-    		self.charlen.setText('1')
+        if int(self.charlen.text()) > 2:
+            self.charlen.setText(str(int(self.charlen.text()) - 1))
+        else:
+            self.charlen.setText('1')
 
     def Generate(self):
-    	if int(self.charlen.text()) > maxchar:
-    		self.charlen.setText(str(maxchar))
+        if int(self.charlen.text()) > maxchar:
+            self.charlen.setText(str(maxchar))
 
-    	if int(self.charlen.text()) < 2:
-    		self.charlen.setText('1')
+        if int(self.charlen.text()) < 2:
+            self.charlen.setText('1')
 
-    	password = ''
+        password = ''
 
-    	for i in range(int(self.charlen.text())):
-    		password += random.choice(char)
+        for i in range(int(self.charlen.text())):
+            password += random.choice(char)
 
-    	self.passwordout.setPlainText(password)
-
-
+        self.passwordout.setPlainText(password)
 
     def copy(self):
-    	cb = QApplication.clipboard()
-    	cb.clear(mode=cb.Clipboard )
-    	cb.setText(self.passwordout.toPlainText(), mode=cb.Clipboard)
-    	
-
+        cb = QApplication.clipboard()
+        cb.clear(mode=cb.Clipboard)
+        cb.setText(self.passwordout.toPlainText(), mode=cb.Clipboard)
 
 
 def main():
